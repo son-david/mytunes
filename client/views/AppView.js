679e551067181563924a7ffc4d80a6764da4ee1c
ammarm08 <ammar.mian08@gmail.com>
2015-10-21 14:05:08 -0700

    queuing songs and rendering them in separate queue table

--- client/views/AppView.js
+++ client/views/AppView.js
@@ -4,18 +4,24 @@ var AppView = Backbone.View.extend({
   initialize: function(params) {
     this.playerView = new PlayerView({model: this.model.get('currentSong')});
     this.libraryView = new LibraryView({collection: this.model.get('library')});
+    this.queueView = new SongQueueView({collection: this.model.get('songQueue')});
 
     // change:currentSong - this is Backbone's way of allowing you to filter events to
     // ONLY receive change events for the specific property, 'currentSong'
     this.model.on('change:currentSong', function(model) {
       this.playerView.setSong(model.get('currentSong'));
     }, this);
+
+    this.model.on('queued:songQueue', function(model) {
+      console.log("now");
+    })
   },
 
   render: function() {
     return this.$el.html([
       this.playerView.$el,
-      this.libraryView.$el
+      this.libraryView.$el,
+      this.queueView.$el
     ]);
   }
 
